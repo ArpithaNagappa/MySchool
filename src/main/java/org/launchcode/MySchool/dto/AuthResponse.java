7b907cffd92276a4cd5bf60ece319f37af819e78
Arpitha Nagappa <arpithagn021@gmail.com>
2026-01-27 23:05:47 -0600

    Updates to login page

---
+++ src/main/java/org/launchcode/MySchool/dto/AuthResponse.java
@@ -0,0 +1,12 @@
+package org.launchcode.MySchool.dto;
+
+public class AuthResponse {
+
+    private String token;
+    private String role;
+
+    public AuthResponse(String token, String name) {
+    }
+
+    /* constructor/getters */
+}
