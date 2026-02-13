8c18a779a42d7eba04a980cdb9ca857ad0e80754
Arpitha Nagappa <arpithagn021@gmail.com>
2026-02-12 22:52:55 -0600

    App.jsx update

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,11 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/events" element={<Events />} />
+         <Route path="/events" element={<Events />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} /> 
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
